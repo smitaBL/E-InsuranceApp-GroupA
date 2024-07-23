7f023a2d51669b7d3d76e10bdf601535eb8b11eb
Renuka <renu.bagave1901@gmail.com>
2024-07-23 12:36:40 +0530

    [Renuka] Refactored : Modified Utility Classes code

--- ModelLayer/EmailML.cs
+++ ModelLayer/EmailML.cs
@@ -9,6 +9,6 @@ namespace ModelLayer
     public class EmailML
     {
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string Password { get; set; }  
     }
 }
