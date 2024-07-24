b82e345742aae1d40414ff375bc2782888560fce
Vikrant-14 <vikrantgawale123@gmail.com>
2024-07-24 11:37:57 +0530

    [Vikrant] Refactored : commit cached files

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf132ac465551c3e14babf755c59d40fbb46ba90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
