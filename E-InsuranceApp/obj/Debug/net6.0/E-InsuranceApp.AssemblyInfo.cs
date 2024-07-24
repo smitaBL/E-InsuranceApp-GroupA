a476412ce4a2e570f9b331ebeb60292e9e9bbb6a
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-07-24 12:13:22 +0530

    [Sakshi] refactored : modified utility classes

--- E-InsuranceApp/obj/Debug/net6.0/E-InsuranceApp.AssemblyInfo.cs
+++ E-InsuranceApp/obj/Debug/net6.0/E-InsuranceApp.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-InsuranceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf132ac465551c3e14babf755c59d40fbb46ba90")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f330179a08fe52a6b59be28f675c16fcfe0f44")]
->>>>>>> Sakshi/Agent-EmployeeRegistration
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bfd59611a6fa3c7c122cc1cd1e586dba8c3e79b")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-InsuranceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-InsuranceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
