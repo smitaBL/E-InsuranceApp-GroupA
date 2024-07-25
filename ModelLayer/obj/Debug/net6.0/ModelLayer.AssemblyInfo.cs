892d7aea2834d08df6591170a752ce787ee218f4
Renuka <renu.bagave1901@gmail.com>
2024-07-25 11:16:18 +0530

    [Renuka] Refactored :  Added Cache Files

--- ModelLayer/obj/Debug/net6.0/ModelLayer.AssemblyInfo.cs
+++ ModelLayer/obj/Debug/net6.0/ModelLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a566428c8688f5959ab47e54cd49454342f580")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
