98e160a199ad07d78ae73aec6d495a5e34a66f93
SakshiKorgaonkar <korgaonkarsakshi23@gmail.com>
2024-07-27 12:26:50 +0530

    [Sakshi] Refactored : committed cached files

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0cc5943e3dea9244610a2fa229e2ea63bed55d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc6bee8999ad81696e7ac483ad24dc2e7626d395")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EmailConsumerApp/obj/Debug/net6.0/EmailConsumerApp.AssemblyInfo.cs
+++ EmailConsumerApp/obj/Debug/net6.0/EmailConsumerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmailConsumerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c5c763d72ee32bbf26f4c6a130ce2b518cfbede")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc6bee8999ad81696e7ac483ad24dc2e7626d395")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmailConsumerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmailConsumerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
